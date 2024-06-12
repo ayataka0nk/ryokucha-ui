54f0107c7ff0068d63dae335475b199f97ac0c0e
Naoto Kamba <naoto.kamba@outlook.jp>
2024-06-12 16:52:50 +0900

    wip

--- packages/core/src/layout/Layout/index.tsx
+++ packages/core/src/layout/Layout/index.tsx
@@ -1,7 +1,8 @@
 import { Outlet } from 'react-router-dom'
 import styles from './styles.module.scss'
 import { UserNavigations } from '../UserNavigation'
-import { NavigationActionType } from '@/Navigation'
+import { NavigationActionType, NavigationTopAppBar } from '@/Navigation'
+import { Logo } from '../UserNavigation/Logo'
 
 export const Layout = () => {
   const navigationAction: NavigationActionType = {
@@ -13,6 +14,7 @@ export const Layout = () => {
     <div className={`${styles['root']} light`}>
       <UserNavigations pageKey="AppBar" navigationAction={navigationAction} />
       <div className={styles['content']}>
+        <NavigationTopAppBar logo={<Logo />} />
         <Outlet />
       </div>
     </div>
